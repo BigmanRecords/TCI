8cf0e03f16e746e0f63d29370bddc7db642c4b2e
BigmanRecords <garethforshaw@hotmail.com>
2019-05-05 16:14:52 +0200

    first test class added for MyFirstClass

---
+++ src/test/java/MyFirstClassTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MyFirstClassTest {
+
+    @Test
+    public void getNumber() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void getSecondnumber() {
+    }
+}
