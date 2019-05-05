5492316d7e066f8e8743e807462f37fb05f66c96
BigmanRecords <garethforshaw@hotmail.com>
2019-05-06 01:21:19 +0200

    chapter test exception thrown if number in wrong format TDD step 2

--- src/main/java/Assignment4/Chapter.java
+++ src/main/java/Assignment4/Chapter.java
@@ -8,6 +8,17 @@ public class Chapter {
     public Chapter(String name, String number){
 
         setName(name);
+
+        int count = 0;
+
+        for(int i = 0; i < number.length(); i++){
+            if(number.charAt(i) == '.')
+                count++;
+        }
+
+        if(count > 1)
+            throw new IllegalArgumentException("name cannot be null");
+
         this.number = number;
     }
 
